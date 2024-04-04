297c5f808b1ad9864b67bed6a44b2f77d9667acf
maruf-ahmed <marufahamed1110@gmail.com>
2024-04-04 23:06:33 +0600

    added rignt nav section

--- src/Components/LeftSideNav.jsx
+++ src/Components/LeftSideNav.jsx
@@ -1,11 +1,29 @@
-
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 function LeftSideNav() {
+  const [category, setCategory] = useState([]);
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategory(data));
+  }, []);
   return (
-    <div>
-      <h1>left side nav</h1>
+    <div className="">
+      <h1 className="text-2xl font-semibold">All Category</h1>
+      <div className="mt-6">
+        {category.map((item, index) => (
+          <NavLink
+            to={item?.id}
+            className="block py-2 mb-3 px-4 font-medium text-md rounded-lg bg-gray-50 hover:bg-gray-100"
+            key={index}
+          >
+            {item?.name}
+          </NavLink>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default LeftSideNav
+export default LeftSideNav;
--- src/Components/RightSideNav.jsx
+++ src/Components/RightSideNav.jsx
@@ -3,11 +3,11 @@ function RightSideNav() {
     <div className="">
       <h1 className="text-2xl font-semibold pb-3">Login With</h1>
       <div className="flex flex-col space-y-3">
-        <button className="p-2 border flex items-center justify-center gap-3 rounded-lg bg-gray-100 hover:bg-gray-200 duration-150">
+        <button className="p-2 border flex items-center justify-center gap-3 font-medium rounded-lg bg-gray-100 hover:bg-gray-200 duration-150">
           <img className="w-6" src="assets/google.png" alt="" /> Login With
           Google
         </button>
-        <button className="p-2 border flex items-center justify-center gap-3 rounded-lg bg-gray-100 hover:bg-gray-200 duration-150">
+        <button className="p-2 border flex items-center justify-center gap-3 font-medium rounded-lg bg-gray-100 hover:bg-gray-200 duration-150">
           <img className="w-6" src="assets/github.png" alt="" /> Login With
           Github
         </button>
@@ -17,13 +17,13 @@ function RightSideNav() {
 
         <a
           href="#"
-          className="p-4 hover:bg-gray-200 duration-150 border-t-2  border-x-2 flex items-center rounded-t-lg gap-3  bg-gray-100"
+          className="p-4 hover:bg-gray-200 duration-150 border-t-2 font-medium  border-x-2 flex items-center rounded-t-lg gap-3  bg-gray-100"
         >
           <i className="ri-facebook-circle-fill text-2xl"></i> Facebook
         </a>
         <a
           href="#"
-          className="p-4 hover:bg-gray-200 duration-150 border-2 flex items-center  gap-3  bg-gray-100"
+          className="p-4 hover:bg-gray-200 duration-150 border-2 font-medium flex items-center  gap-3  bg-gray-100"
         >
           {" "}
           <i className="ri-instagram-fill text-2xl"></i>
@@ -31,7 +31,7 @@ function RightSideNav() {
         </a>
         <a
           href="#"
-          className="p-4 hover:bg-gray-200 duration-150 border-b-2 border-x-2 flex items-center gap-3 rounded-b-lg bg-gray-100"
+          className="p-4 hover:bg-gray-200 duration-150 border-b-2 font-medium border-x-2 flex items-center gap-3 rounded-b-lg bg-gray-100"
         >
           {" "}
           <i className="ri-twitter-x-fill text-2xl"></i> Twitter
